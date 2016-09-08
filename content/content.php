1037832ad028b980a5385fff4d4b82e0b9a7e2e0
jKratzik <info@zoomdrive.at>
2016-09-08 15:58:34 +0200

    Removed whitespace and HTML comments

--- content/content.php
+++ content/content.php
@@ -3,27 +3,20 @@
 		<header class="entry-header">
 			<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>            
             <?php locate_template( array( 'misc/entry-byline.php' ), true, false ); // Loads the misc/entry-byline.php template. ?>
-		</header><!-- .entry-header -->
-
+		</header>
 		<div <?php hybrid_attr( 'entry-content' ); ?>>
 			<?php the_content(); ?>
 			<?php wp_link_pages(); ?>
-		</div><!-- .entry-content -->
-
+		</div>
 		<footer class="entry-footer">
 			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'text' => esc_html__( 'Posted in %s', 'toolbox' ) ) ); ?>
 			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'text' => esc_html__( 'Tagged with %s', 'toolbox' ), 'before' => '<br />' ) ); ?>
-		</footer><!-- .entry-footer -->
-
+		</footer>
 	<?php else : // If not viewing a single post. ?>
-	
 		<?php locate_template( array( 'misc/non-singular-content.php' ), true, false ); // Loads the misc/non-singular-content.php template. ?>
-		
 		<footer class="entry-footer">
 			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'text' => esc_html__( 'Posted in %s', 'toolbox' ) ) ); ?>
 			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'text' => esc_html__( 'Tagged with %s', 'toolbox' ), 'before' => '<br />' ) ); ?>
-		</footer><!-- .entry-footer -->
-
+		</footer>
 	<?php endif; // End single post check. ?>
-
-</article><!-- .entry -->
+</article>
