8322ce9009c69bb006c0fe5815a88b961d2b9656
jKratzik <info@zoomdrive.at>
2016-09-08 17:06:43 +0200

    Removed whitespace

--- comment.php
+++ comment.php
@@ -1,5 +1,4 @@
 <li <?php hybrid_attr( 'comment' ); ?>>
-
 	<article>
 		<header class="media-object comment-meta">
 			<p class="media-object-section comment-avatar">
@@ -10,13 +9,10 @@
 				<time <?php hybrid_attr( 'comment-published' ); ?>><?php printf( esc_html__( '%s ago', 'toolbox' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ); ?></time>
 				<?php edit_comment_link( __( 'Edit', 'toolbox' ) ); ?>
 			</p>
-		</header><!-- .comment-meta -->
-
+		</header>
 		<div <?php hybrid_attr( 'comment-content' ); ?>>
 			<?php comment_text(); ?>
-		</div><!-- .comment-content -->
-
+		</div>
 		<?php hybrid_comment_reply_link(); ?>
 	</article>
-
 <?php // No closing </li> is needed.  WordPress will know where to add it. ?>
