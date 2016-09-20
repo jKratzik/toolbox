22e6755a83913a8f05a76131e2f7971226216161
jKratzik <info@zoomdrive.at>
2016-09-20 17:32:27 +0200

    Cleanup

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function( $ ) {
 	
-	$('#container').wrap(
+	$( '.site-container' ).wrap(
 		'<div class="off-canvas-wrapper"><div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>'
 	)
-	.addClass('off-canvas-content')
-	.attr('data-off-canvas-content', '')
+	.addClass( 'off-canvas-content' )
+	.attr( 'data-off-canvas-content', '' )
 	.prepend('\
 		<div class="title-bar hide-for-large mobile-toggle">\
 			<div class="title-bar-left" data-toggle="mobile-nav">\
@@ -13,17 +13,18 @@ jQuery(document).ready(function($) {
 			</div>\
 		</div>');
 	
-	$('.off-canvas-wrapper-inner').prepend(
+	$( '.off-canvas-wrapper-inner' ).prepend(
 		'<div class="off-canvas position-left" data-force-top="false" id="mobile-nav" data-off-canvas><ul class="vertical menu" data-drilldown data-parent-link="true">'
 	);
 	
-	$('#menu-primary-items > li, #menu-secondary-items > li' ).clone().appendTo('#mobile-nav > ul');
-	
-	$(document).foundation();
+	$( '.clone-mobile' ).each( function() {
+		$( '> li', this).clone().appendTo('#mobile-nav > ul' );
+    });
 		
-	//$( '<li class="submenu-first"></li>' ).insertAfter( '.js-drilldown-back' );
-	$('.menu-item-has-children').each(function() {
-		( $('> .sub-menu > .js-drilldown-back a', this).text( $('> a', this).text() ) );
+	$( document ).foundation();
+		
+	$( '.menu-item-has-children' ).each( function() {
+		( $( '> .sub-menu > .js-drilldown-back a', this ).text( $( '> a', this ).text() ) );
     });
 	
 });
