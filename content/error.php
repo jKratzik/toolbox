b226a608881f19e860fb73313494d8ab5dc83bd3
jKratzik <info@zoomdrive.at>
2016-09-20 16:08:31 +0200

    Added comments

--- content/error.php
+++ content/error.php
@@ -1,5 +1,5 @@
 <article class="entry">
-	<?php if ( is_404() ) : ?>
+	<?php if ( is_404() ) :  // If page is not found. ?>
 		<header class="entry-header">
 			<h1 class="entry-title"><?php esc_html_e( 'This page doesn\'t exist', 'toolbox' ); ?></h1>
 		</header>
@@ -7,7 +7,7 @@
 			<p><?php printf( __( 'Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it by using the search form below.', 'toolbox' ), trailingslashit( home_url() ) ); ?></p>
 			<?php get_search_form(); ?>
 		</div>
-	<?php else : ?>
+	<?php else :  // If no entries are found. ?>
 		<div <?php hybrid_attr( 'entry-content' ); ?>>
 			<?php esc_html_e( 'Sorry, no entries have been found.', 'toolbox' ); ?>
 		</div>
